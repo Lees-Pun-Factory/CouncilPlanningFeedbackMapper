2abb3474f97dfb74892385d16885ec617658eeaa
lee-yeomans <lyomans@icloud.com>
2018-11-17 20:29:58 +0000

    Created scraper component

---
+++ feedback_mapper/src/scraper/scraper.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Scraper extends React.Component {
+
+	componentDidMount(){
+		// PUT CODE IN HERE CLARE!!!!!!!!!!!!!!!!!!!
+	}
+
+	render() {
+		return <h1>Hello {this.props.name}</h1>;
+	}
+}
